a5698dcff3e16a65ac1c6f96d23b6dd9f6224036
ottojolanki <ojolanki@stanford.edu>
2018-09-13 10:32:39 -0700

    continue test refactor

--- test/test_workflow/test.sh
+++ test/test_workflow/test.sh
@@ -23,20 +23,22 @@ fi
 
 CROMWELL_JAR=cromwell-34.jar
 BACKEND_CONF=backends/backend.conf
-BACKEND=Local
 RESULT_PREFIX=$(basename ${INPUT} .json)
 METADATA=${RESULT_PREFIX}.metadata.json # metadata
 RESULT=${RESULT_PREFIX}.result.json # output
 
-# Write workflow option JSON file
-TMP_WF_OPT=$RESULT_PREFIX.test_rna_wf_opt.json
-cat > $TMP_WF_OPT << EOM
-{
-    "default_runtime_attributes" : {
-        "docker" : "$DOCKER_IMAGE"
+if [ $4 = "docker" ]; then
+    # Write workflow option JSON file
+    BACKEND=Local
+    TMP_WF_OPT=$RESULT_PREFIX.test_rna_wf_opt.json
+    cat > $TMP_WF_OPT << EOM
+    {
+        "default_runtime_attributes" : {
+            "docker" : "$DOCKER_IMAGE"
+        }
     }
-}
-EOM
+    EOM
+fi
 
 java -Dconfig.file=${BACKEND_CONF} -Dbackend.default=${BACKEND} -jar ${CROMWELL_JAR} run ${WDL} -i ${INPUT} -o ${TMP_WF_OPT} -m ${METADATA}
 
