99035d769436974b0dc4a126efccdae5c07b8a45
ottojolanki <ojolanki@stanford.edu>
2018-10-25 11:34:15 -0700

    rna_qc python script and bam handling

--- src/rna_qc.py
+++ src/rna_qc.py
@@ -10,9 +10,9 @@ __license__ = 'MIT'
 import argparse
 import json
 import logging
+import pysam
 from bisect import insort
 from collections import OrderedDict
-from pysam import AlignmentFile
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -150,6 +150,67 @@ def read_dict_from_tsv(path_to_tsv):
     with open(path_to_tsv, 'rt') as f:
         for line in f:
             line_split = line.split()
-            assert len(line_split) == 2, 'Malformed line: {}'.format(line)
+            try:
+                assert len(line_split) == 2, 'Malformed line: {}'.format(line)
+            except AssertionError:
+                logger.exception('Malformed line %s', line)
+                raise
             result.update({line_split[0]: line_split[1]})
     return result
+
+
+def get_gene_type_counts(tr_to_gene_type_map, bampath):
+    """Counts reads by gene type from transcriptome alignment .bam file.
+
+    Args:
+        tr_to_gene_type_map: dict that maps transcript ids to gene types
+        bampath: file path to transcriptome alignment .bam
+
+    Returns:
+        reads_by_gene_type: dict with counts of reads by gene type
+
+    Raises:
+        KeyError if transcript id (read.reference_name) that is in the .bam
+        is not found in the tr_to_gene_type_map. The intended use of this
+        function is to use the mapping generated from the same annotation
+        that was used to build the bam, so this is an indication of mismatched
+        inputs.
+    """
+    bamfile = pysam.AlignmentFile(bampath, 'rb')
+    reads_by_gene_type = {key: 0 for key in set(tr_to_gene_type_map.values())}
+    for read in bamfile.fetch(until_eof=True):
+        if read.is_secondary or read.is_unmapped or read.is_qcfail or read.is_duplicate:
+            continue
+        else:
+            reference_name = read.reference_name
+            gene_type = tr_to_gene_type_map[reference_name]
+            reads_by_gene_type[gene_type] += 1
+    return reads_by_gene_type
+
+
+def main(args):
+    qc_record = QCMetricRecord()
+    logger.info('Reading transcript id to gene type mapping from %s',
+                args.tr_id_to_gene_type_tsv)
+    tr_to_gene_type_map = read_dict_from_tsv(args.tr_id_to_gene_type_tsv)
+    logger.info('Calculating gene type counts for bam %s', args.input_bam)
+    gene_type_counts = get_gene_type_counts(tr_to_gene_type_map,
+                                            args.input_bam)
+    gene_type_counts = QCMetric('gene_type_count', gene_type_counts)
+    qc_record.add(gene_type_counts)
+    logger.info('Writing QC output into %s', args.output_filename)
+    with open(args.output_filename, 'wt') as fp:
+        json.dump(qc_record.to_ordered_dict(), fp)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_bam', type=str, help='path to transcriptome bam')
+    parser.add_argument(
+        '--tr_id_to_gene_type_tsv',
+        type=str,
+        help='path to transcript id to gene type tsv')
+    parser.add_argument('--output_filename', type=str)
+    args = parser.parse_args()
+    main(args)
