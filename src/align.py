6db92fd42e9dbd04bcf31f714167d15ebc4427df
ottojolanki <ojolanki@stanford.edu>
2019-09-17 14:20:01 -0700

    make it black

--- src/align.py
+++ src/align.py
@@ -3,9 +3,9 @@
 Script to run alignment(mapping) step in ENCODE rna-seq-pipeline
 """
 
-__author__ = 'Otto Jolanki'
-__version__ = '0.1.0'
-__license__ = 'MIT'
+__author__ = "Otto Jolanki"
+__version__ = "0.1.0"
+__license__ = "MIT"
 
 from abc import ABC, abstractmethod
 from qc_utils import QCMetric
@@ -24,12 +24,11 @@ import tarfile
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-filehandler = logging.FileHandler('align.log')
+filehandler = logging.FileHandler("align.log")
 filehandler.setLevel(logging.DEBUG)
 consolehandler = logging.StreamHandler()
 consolehandler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s | %(levelname)s | %(name)s: %(message)s')
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s: %(message)s")
 filehandler.setFormatter(formatter)
 consolehandler.setFormatter(formatter)
 logger.addHandler(consolehandler)
@@ -37,17 +36,15 @@ logger.addHandler(filehandler)
 
 
 def make_aligner(endedness, fastqs, ncpus, ramGB, indexdir):
-    if endedness == 'single':
-        logger.info('Creating a single-ended aligner.')
-        return SingleEndedStarAligner(fastqs, ncpus, ramGB,
-                                      indexdir)
-    elif endedness == 'paired':
-        logger.info('Creating a paired-end aligner.')
-        return PairedEndStarAligner(fastqs, ncpus, ramGB,
-                                    indexdir)
+    if endedness == "single":
+        logger.info("Creating a single-ended aligner.")
+        return SingleEndedStarAligner(fastqs, ncpus, ramGB, indexdir)
+    elif endedness == "paired":
+        logger.info("Creating a paired-end aligner.")
+        return PairedEndStarAligner(fastqs, ncpus, ramGB, indexdir)
 
 
-def make_modified_TarInfo(archive, target_dir=''):
+def make_modified_TarInfo(archive, target_dir=""):
     """
     Input: archive TarFile object
     Input: target_dir string
@@ -65,8 +62,7 @@ def make_modified_TarInfo(archive, target_dir=''):
     members = []
     for member in archive.getmembers():
         if member.isfile():
-            member.name = os.path.join(target_dir,
-                                       os.path.basename(member.name))
+            member.name = os.path.join(target_dir, os.path.basename(member.name))
             members.append(member)
     return members
 
@@ -85,7 +81,7 @@ def get_tmp_file_name(extension=".fastq.gz"):
     will not collide with anything.
     """
     while True:
-        tmp_name = ''.join(choices('abcdefghijklmnopqrstuvwxyz', k=20)) + extension
+        tmp_name = "".join(choices("abcdefghijklmnopqrstuvwxyz", k=20)) + extension
         if pathlib.Path(tmp_name).exists():
             continue
         else:
@@ -111,10 +107,10 @@ def concatenate_files(input_files):
 
 
 def get_flagstats(input_path, output_path):
-    command = 'samtools flagstat {infile}'.format(infile=input_path)
-    logger.info('Getting samtools flagstats for %s', input_path)
+    command = "samtools flagstat {infile}".format(infile=input_path)
+    logger.info("Getting samtools flagstats for %s", input_path)
     process = subprocess.run(shlex.split(command), stdout=subprocess.PIPE)
-    with open(output_path, 'w') as f:
+    with open(output_path, "w") as f:
         f.write(process.stdout.decode())
     return None
 
@@ -137,7 +133,7 @@ class StarAligner(ABC):
         self.indexdir = indexdir
 
     def run(self):
-        logger.info('running STAR command %s', ' '.join(self.command))
+        logger.info("running STAR command %s", " ".join(self.command))
         subprocess.call(self.command)
 
     @property
@@ -152,7 +148,7 @@ class StarAligner(ABC):
 
 class SingleEndedStarAligner(StarAligner):
 
-    command_string = '''STAR --genomeDir {indexdir} \
+    command_string = """STAR --genomeDir {indexdir} \
     --readFilesIn {infastq} \
     --readFilesCommand zcat \
     --runThreadN {ncpus} \
@@ -174,26 +170,28 @@ class SingleEndedStarAligner(StarAligner):
     --outSAMtype BAM SortedByCoordinate  \
     --quantMode TranscriptomeSAM \
     --sjdbScore 1 \
-    --limitBAMsortRAM {ramGB}000000000'''
+    --limitBAMsortRAM {ramGB}000000000"""
 
     def __init__(self, fastqs, ncpus, ramGB, indexdir):
         super().__init__(ncpus, ramGB, indexdir)
         self.input_fastq = fastqs[0]
         self.command = shlex.split(
-            self.format_command_string(type(self).command_string))
+            self.format_command_string(type(self).command_string)
+        )
 
     def format_command_string(self, input_string):
         cmd = input_string.format(
             infastq=self.input_fastq,
             ncpus=self.ncpus,
             ramGB=self.ramGB,
-            indexdir=self.indexdir)
+            indexdir=self.indexdir,
+        )
         return cmd
 
 
 class PairedEndStarAligner(StarAligner):
 
-    command_string = '''STAR --genomeDir {indexdir} \
+    command_string = """STAR --genomeDir {indexdir} \
     --readFilesIn {read1_fq_gz} {read2_fq_gz} \
     --readFilesCommand zcat \
     --runThreadN {ncpus} \
@@ -214,14 +212,15 @@ class PairedEndStarAligner(StarAligner):
     --outSAMtype BAM SortedByCoordinate \
     --quantMode TranscriptomeSAM \
     --sjdbScore 1 \
-    --limitBAMsortRAM {ramGB}000000000'''
+    --limitBAMsortRAM {ramGB}000000000"""
 
     def __init__(self, fastqs, ncpus, ramGB, indexdir):
         super().__init__(ncpus, ramGB, indexdir)
         self.fastq_read1 = fastqs[0]
         self.fastq_read2 = fastqs[1]
         self.command = shlex.split(
-            self.format_command_string(type(self).command_string))
+            self.format_command_string(type(self).command_string)
+        )
 
     def format_command_string(self, input_string):
         cmd = input_string.format(
@@ -229,7 +228,8 @@ class PairedEndStarAligner(StarAligner):
             read2_fq_gz=self.fastq_read2,
             ncpus=self.ncpus,
             ramGB=self.ramGB,
-            indexdir=self.indexdir)
+            indexdir=self.indexdir,
+        )
         return cmd
 
 
@@ -247,88 +247,93 @@ def main(args):
 
     if merged_R2 and args.endedness == "paired":
         fastqs.append(merged_R2)
-    with tarfile.open(args.index, 'r:gz') as archive:
+    with tarfile.open(args.index, "r:gz") as archive:
         archive.extractall()
-    aligner = make_aligner(args.endedness, fastqs, args.ncpus, args.ramGB, args.indexdir)
+    aligner = make_aligner(
+        args.endedness, fastqs, args.ncpus, args.ramGB, args.indexdir
+    )
     aligner.run()
     cwd = os.getcwd()
-    genome_bam_path = os.path.join(cwd, args.bamroot + '_genome.bam')
-    anno_bam_path = os.path.join(cwd, args.bamroot + '_anno.bam')
-    genome_flagstat_path = os.path.join(cwd,
-                                        args.bamroot + '_genome_flagstat.txt')
-    anno_flagstat_path = os.path.join(cwd, args.bamroot + '_anno_flagstat.txt')
-    star_log_path = os.path.join(cwd, args.bamroot + '_Log.final.out')
-    os.rename(
-        os.path.join(cwd, 'Aligned.sortedByCoord.out.bam'),
-        genome_bam_path)
-    os.rename(
-        os.path.join(cwd, 'Log.final.out'),
-        star_log_path)
-    rsem_check_cmd = 'rsem-sam-validator {bam_to_check}'.format(
-        bam_to_check='Aligned.toTranscriptome.out.bam')
+    genome_bam_path = os.path.join(cwd, args.bamroot + "_genome.bam")
+    anno_bam_path = os.path.join(cwd, args.bamroot + "_anno.bam")
+    genome_flagstat_path = os.path.join(cwd, args.bamroot + "_genome_flagstat.txt")
+    anno_flagstat_path = os.path.join(cwd, args.bamroot + "_anno_flagstat.txt")
+    star_log_path = os.path.join(cwd, args.bamroot + "_Log.final.out")
+    os.rename(os.path.join(cwd, "Aligned.sortedByCoord.out.bam"), genome_bam_path)
+    os.rename(os.path.join(cwd, "Log.final.out"), star_log_path)
+    rsem_check_cmd = "rsem-sam-validator {bam_to_check}".format(
+        bam_to_check="Aligned.toTranscriptome.out.bam"
+    )
     rsem_output = subprocess.check_output(shlex.split(rsem_check_cmd))
     # rsem validator exits with 0 whether the check passes or not
     # for this reason we check if the output ends in 'is valid!'
     # the other possibility is 'is not valid!'
-    rsem_valid = rsem_output.decode().strip().split('\n')[-1].endswith(
-        'is valid!')
+    rsem_valid = rsem_output.decode().strip().split("\n")[-1].endswith("is valid!")
     if rsem_valid:
-        logger.info('Transcriptome bam is already rsem-sorted.')
-        os.rename(
-            os.path.join(cwd, 'Aligned.toTranscriptome.out.bam'),
-            anno_bam_path)
+        logger.info("Transcriptome bam is already rsem-sorted.")
+        os.rename(os.path.join(cwd, "Aligned.toTranscriptome.out.bam"), anno_bam_path)
     else:
-        logger.info('Transcriptome bam is not rsem-sorted.')
-        rsem_sort_cmd = 'convert-sam-for-rsem {input} {output}'.format(
-            input='Aligned.toTranscriptome.out.bam',
-            output=args.bamroot + '_anno')
-        logger.info('Running %s', rsem_sort_cmd)
+        logger.info("Transcriptome bam is not rsem-sorted.")
+        rsem_sort_cmd = "convert-sam-for-rsem {input} {output}".format(
+            input="Aligned.toTranscriptome.out.bam", output=args.bamroot + "_anno"
+        )
+        logger.info("Running %s", rsem_sort_cmd)
         subprocess.call(shlex.split(rsem_sort_cmd))
     get_flagstats(genome_bam_path, genome_flagstat_path)
     get_flagstats(anno_bam_path, anno_flagstat_path)
     anno_flagstat_content = parse_flagstats(anno_flagstat_path)
     genome_flagstat_content = parse_flagstats(genome_flagstat_path)
     star_log_content = parse_starlog(star_log_path)
-    anno_flagstat_qc = QCMetric('samtools_anno_flagstat', anno_flagstat_content)
-    genome_flagstat_qc = QCMetric('samtools_genome_flagstat', genome_flagstat_content)
-    star_log_qc = QCMetric('star_log_qc', star_log_content)
-    write_json(anno_flagstat_qc.to_ordered_dict(), re.sub(r"\.txt$", ".json", anno_flagstat_path))
-    write_json(genome_flagstat_qc.to_ordered_dict(), re.sub(r"\.txt$", ".json", genome_flagstat_path))
+    anno_flagstat_qc = QCMetric("samtools_anno_flagstat", anno_flagstat_content)
+    genome_flagstat_qc = QCMetric("samtools_genome_flagstat", genome_flagstat_content)
+    star_log_qc = QCMetric("star_log_qc", star_log_content)
+    write_json(
+        anno_flagstat_qc.to_ordered_dict(),
+        re.sub(r"\.txt$", ".json", anno_flagstat_path),
+    )
+    write_json(
+        genome_flagstat_qc.to_ordered_dict(),
+        re.sub(r"\.txt$", ".json", genome_flagstat_path),
+    )
     write_json(star_log_qc.to_ordered_dict(), re.sub(r"\.out$", ".json", star_log_path))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fastqs_R1', nargs='+', help='Input gzipped fastq(s) belonging to read1')
-    parser.add_argument('--fastqs_R2', nargs='*', help='Input gzipped fastq(s) belonging to read2')
     parser.add_argument(
-        '--index',
-        type=str,
-        help='Path to aligner index tar.gz archive.',
-        required=True)
+        "--fastqs_R1", nargs="+", help="Input gzipped fastq(s) belonging to read1"
+    )
     parser.add_argument(
-        '--indexdir',
-        type=str,
-        help='Directory to extract index to.',
-        default='out')
+        "--fastqs_R2", nargs="*", help="Input gzipped fastq(s) belonging to read2"
+    )
     parser.add_argument(
-        '--endedness',
-        type=str,
-        choices=['paired', 'single'],
-        help='paired or single',
-        required=True)
+        "--index", type=str, help="Path to aligner index tar.gz archive.", required=True
+    )
     parser.add_argument(
-        '--bamroot',
+        "--indexdir", type=str, help="Directory to extract index to.", default="out"
+    )
+    parser.add_argument(
+        "--endedness",
         type=str,
-        help='''
+        choices=["paired", "single"],
+        help="paired or single",
+        required=True,
+    )
+    parser.add_argument(
+        "--bamroot",
+        type=str,
+        help="""
              Root name for output bams. For example out_bam
              will create out_bam_genome.bam and out_bam_anno.bam
-             ''',
-        default='out_bam')
+             """,
+        default="out_bam",
+    )
     parser.add_argument(
-        '--ncpus', type=int, help='Number of cpus available.', default=4)
+        "--ncpus", type=int, help="Number of cpus available.", default=4
+    )
     parser.add_argument(
-        '--ramGB', type=int, help='Amount of RAM available in GB.', default=8)
+        "--ramGB", type=int, help="Amount of RAM available in GB.", default=8
+    )
 
     args = parser.parse_args()
     main(args)
--- src/merge_annotation.py
+++ src/merge_annotation.py
@@ -20,30 +20,30 @@ def replace_nth_position_with(input_string, position, replacement, separator):
     return separator.join(result)
 
 
-def strip_left_until_and_including(input_string, sentinel='>'):
-    '''
+def strip_left_until_and_including(input_string, sentinel=">"):
+    """
     input: string
     output: input string - everything on the left, and
             including the sentinel character
     If the sentinel character is not found in the string
     a ValueError is raised.
-    '''
-    return input_string[input_string.index(sentinel) + 1:]
+    """
+    return input_string[input_string.index(sentinel) + 1 :]
 
 
 def get_fasta_tokens(fastastring):
-    '''
+    """
     input: .fasta file as string
     output: list of strings, each string representing a token in .fasta
             with everything before, and including first occurence of '>'
             removed from each.
-    '''
+    """
     lstripped_fastastring = strip_left_until_and_including(fastastring)
-    return lstripped_fastastring.split('>')
+    return lstripped_fastastring.split(">")
 
 
 def remove_whitespace(input_string):
-    return ''.join(input_string.split())
+    return "".join(input_string.split())
 
 
 if __name__ == "__main__":
@@ -55,67 +55,75 @@ if __name__ == "__main__":
         "--annotation",
         action="store",
         required=True,
-        help='Gencode annotation .gtf file in gzipped format.')
+        help="Gencode annotation .gtf file in gzipped format.",
+    )
 
     # tRNA filename
     parser.add_argument(
         "--tRNA",
         action="store",
         required=True,
-        help='Gencode tRNA .gtf file in gzipped format.')
+        help="Gencode tRNA .gtf file in gzipped format.",
+    )
 
     # spikeins filename
     parser.add_argument(
         "--spikeins",
         action="store",
         required=True,
-        help='Spikein .fasta file in gzipped format.')
+        help="Spikein .fasta file in gzipped format.",
+    )
 
     parser.add_argument(
         "--output_filename",
         action="store",
         required=True,
-        help='Output filename, including .gz ending.')
+        help="Output filename, including .gz ending.",
+    )
 
     # Specify output of "--version"
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s (version {version})".format(version=__version__))
+        version="%(prog)s (version {version})".format(version=__version__),
+    )
 
     args = parser.parse_args()
 
     # Process the files starts here
-    annotation_file = gzip.open(args.annotation, 'rt')
-    tRNA_file = gzip.open(args.tRNA, 'rt')
-    spikein_file = gzip.open(args.spikeins, 'rt')
+    annotation_file = gzip.open(args.annotation, "rt")
+    tRNA_file = gzip.open(args.tRNA, "rt")
+    spikein_file = gzip.open(args.spikeins, "rt")
 
     # Only thing that needs to be done to the annotation_file is
     # removing commentlines that start with #
     # Use generators for tRNAs and annotation to save memory
 
-    annotation_linegenerator = (line for line in annotation_file
-                                if not line.startswith('#'))
+    annotation_linegenerator = (
+        line for line in annotation_file if not line.startswith("#")
+    )
     # remove possible comment lines from the beginning of the tRNA file
     # replace tRNAscan column with exon in the result
-    tRNA_nocomments = (line for line in tRNA_file if not line.startswith('#'))
-    tRNA_linegenerator = (replace_nth_position_with(line, 2, 'exon', '\t')
-                          for line in tRNA_nocomments)
+    tRNA_nocomments = (line for line in tRNA_file if not line.startswith("#"))
+    tRNA_linegenerator = (
+        replace_nth_position_with(line, 2, "exon", "\t") for line in tRNA_nocomments
+    )
 
     with spikein_file as f:
         spikein_string = f.read()
 
     spikein_tokens = get_fasta_tokens(spikein_string)
     # split the tokens into sequence of [name, sequence] -pairs
-    spikein_pairs = (token.split('\n', 1) for token in spikein_tokens)
+    spikein_pairs = (token.split("\n", 1) for token in spikein_tokens)
     # get rid of whitespace in nucleotide code, calculate length
-    spikein_name_length_pairs_no_whitespace = ([
-        token[0], len(''.join(token[1].split()))
-    ] for token in spikein_pairs)
+    spikein_name_length_pairs_no_whitespace = (
+        [token[0], len("".join(token[1].split()))] for token in spikein_pairs
+    )
     # build a sequence of formatted spikein lines
-    spikein_lines = (SPIKEIN_FORMAT.format(
-        spikein_name=token[0], sequence_length=token[1])
-                     for token in spikein_name_length_pairs_no_whitespace)
+    spikein_lines = (
+        SPIKEIN_FORMAT.format(spikein_name=token[0], sequence_length=token[1])
+        for token in spikein_name_length_pairs_no_whitespace
+    )
 
     with gzip.open(args.output_filename, "wt") as f_out:
         for line in annotation_linegenerator:
